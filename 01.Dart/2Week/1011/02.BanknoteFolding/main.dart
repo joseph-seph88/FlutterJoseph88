e2bd4b77a0f0e3a88b46b7e8f7b7129b9cb79337
GeonHo Kim <yorjsgh@naver.com>
2024-10-13 15:03:44 +0900

    지폐접기 커밋

---
+++ 01.Dart/2Week/1011/02.BanknoteFolding/main.dart
@@ -0,0 +1,18 @@
+void main() {
+  Solution solution = Solution();
+
+  List<int> wallet = [30, 15];
+  //List<int> wallet = [50, 50];
+  List<int> bill = [26, 17];
+  //List<int> bill = [100, 241];
+
+  print(solution.solution(wallet, bill));
+}
+
+class Solution {
+  int solution(List<int> wallet, List<int> bill) {
+    int answer = 0;
+
+    return answer;
+  }
+}
