b11c366e0684cb0c0b43d20c87b5d8dc3497dd9d
GeonHo Kim <yorjsgh@naver.com>
2024-10-13 20:26:37 +0900

    1011알고리즘 업데이트

--- 01.Dart/2Week/1011/02.BanknoteFolding/01.GeonHo/main.dart
+++ 01.Dart/2Week/1011/02.BanknoteFolding/01.GeonHo/main.dart
@@ -1,10 +1,13 @@
+// wallet	  bill	      result
+// [30, 15]	[26, 17]	  1
+// [50, 50]	[100, 241]	4
 void main() {
   Solution solution = Solution();
 
-  List<int> wallet = [30, 15];
-  //List<int> wallet = [50, 50];
-  List<int> bill = [26, 17];
-  //List<int> bill = [100, 241];
+  //List<int> wallet = [30, 15];
+  List<int> wallet = [50, 50];
+  //List<int> bill = [26, 17];
+  List<int> bill = [100, 241];
 
   print(solution.solution(wallet, bill));
 }
@@ -13,6 +16,23 @@ class Solution {
   int solution(List<int> wallet, List<int> bill) {
     int answer = 0;
 
+    while (!isFit(wallet, bill)) {
+      bill = foldBill(bill);
+      answer++;
+    }
+
     return answer;
+  }
+
+  bool isFit(List<int> wallet, List<int> bill) {
+    return (wallet[0] >= bill[0] && wallet[1] >= bill[1]) || (wallet[0] >= bill[1] && wallet[1] >= bill[0]);
+  }
+
+  List<int> foldBill(List<int> bill) {
+    if (bill[0] >= bill[1]) {
+      return [bill[0] ~/ 2, bill[1]];
+    } else {
+      return [bill[0], bill[1] ~/ 2];
+    }
   }
 }
