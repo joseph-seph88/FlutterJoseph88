7286ccefdd282c31e7362e19976356e7257dd55f
Leeyeji <yeji0718@naver.com>
2024-10-15 19:11:27 +0900

    화이팅

---
+++ 01.Dart/2Week/1011/01.ContainsDuplicate/04. YejiLee/main.dart
@@ -0,0 +1,19 @@
+void main() {
+  List<int> input1 = [1, 2, 3, 1];
+  List<int> input2 = [1, 2, 3, 4];
+  List<int> input3 = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2];
+
+  Solution solution = Solution();
+
+  print(solution.containsDuplicate(input1));
+  print(solution.containsDuplicate(input2));
+  print(solution.containsDuplicate(input3));
+}
+
+class Solution {
+  bool containsDuplicate(List<int> nums) {
+    bool result = true;
+
+    return result;
+  }
+}
