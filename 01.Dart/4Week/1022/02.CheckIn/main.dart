eff63ef326be8810a32c1aa930ac305b3cf78177
GeonHo Kim <yorjsgh@naver.com>
2024-10-22 15:22:19 +0900

    체크인_템플릿

---
+++ 01.Dart/4Week/1022/02.CheckIn/main.dart
@@ -0,0 +1,39 @@
+// /*
+
+// 엘리서 여러분의 체크인 앱 개발
+
+// 엘리서 체크인 시각 관련 클래스 정의
+// -
+
+//  */
+
+// enum Status{
+//   CheckIn,
+//   CheckOut
+// }
+
+// // 1. 엘리서 클래스를 정의
+// class Eliser {
+
+// } 
+
+// // 2. 엘리서의 체크인 시각을 입력하는 클래스를 정의
+// class Checkin {
+//   // 체크인/체크아웃
+
+
+// }
+// void main() {
+
+//   List<Eliser> classMembers3 = [];
+//   Eliser member = Eliser(name : '이예지', className: '', status: Status  );
+//   class3.add(value)
+
+//   member.checkIn()
+//   member.checkOut()
+
+//   // report method
+//   // class3 멤버중에서 특정시간 이후 지각한사람만 뽑아야 할떄 호출하는 메서드
+//   // 한달동안 지각 3회 이상인 사람만 뽑는 메서드
+
+// }
