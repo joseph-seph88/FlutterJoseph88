4f7aad0bb1ad841f660be7153b69409a182e074c
Leeyeji <yeji0718@naver.com>
2024-10-27 15:14:57 +0900

    로마 알고리즘

--- 01.Dart/4Week/1025/03.RomanToInteger/04 Yeji/main.dart
+++ 01.Dart/4Week/1025/03.RomanToInteger/04 Yeji/main.dart
@@ -41,31 +41,42 @@
 
 class Solution {
   int romanToInt(String s) {
-    Map<String, int> mapRoman = {
-      "I": 1,
-      "V": 5,
-      "X": 10,
-      "L": 50,
-      "C": 100,
-      "D": 500,
-      "M": 1000
+    Map<String, int> romanMap = {
+      // 맵으로 키벨류 연결
+      'I': 1,
+      'V': 5,
+      'X': 10,
+      'L': 50,
+      'C': 100,
+      'D': 500,
+      'M': 1000
     };
 
-    int answer = 0;
-    int prev = 0;
+    int num = 0; // 결과값 객체
 
-    for (String roman in s.split('').reversed) {
-      int value = mapRoman[roman]!;
+    // 문자열을 순회
+    for (int i = 0; i < s.length; i++) {
+      int currentValue = romanMap[s[i]]!;
+      // 로드맵의 문자열s의 i번쨰의 값은 currentValue이다.
 
-      if (value >= prev) {
-        answer += value;
+      if (i + 1 < s.length) {
+        int nextValue = romanMap[s[i + 1]]!;
+        // 다음 값 만들어주기
+
+        if (currentValue < nextValue) {
+          // 다음값이 현재의 값보다 크면
+          num -= currentValue;
+          // num에서 현재의 값 더해주기
+        } else {
+          num += currentValue;
+          // 아님 더해주기
+        }
       } else {
-        answer -= value;
+        num += currentValue;
+        //num에다가 더하기
       }
-
-      prev = value;
     }
 
-    return answer;
+    return num;
   }
 }
