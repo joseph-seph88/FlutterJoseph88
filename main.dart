b534242a2a559a8cc7bb5d7ca5df4c8263442c02
Joseph88 <koutz3434@naver.com>
2024-11-01 04:51:39 +0900

    CHECK IN OUT APP

---
+++ main.dart
@@ -0,0 +1,24 @@
+import 'package:check_app_ver02/screens/check_homescreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SubMainVer00());
+}
+
+class SubMainVer00 extends StatelessWidget {
+  const SubMainVer00({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          appBarTheme: AppBarTheme(
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.deepPurple[200],
+          )
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const CheckHomescreen(),
+    );
+  }
+}
