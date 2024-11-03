3feb1866da0f5582cd21dad19c788ce52a07abd5
Joseph88 <koutz3434@naver.com>
2024-11-04 00:40:46 +0900

    Remove mis1

--- 02.Flutter/check_in_out_app/android/app/src/main/kotlin/com/example/check_in_out_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.check_in_out_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- 02.Flutter/egg_time_application/android/app/src/main/kotlin/com/example/egg_time_application/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.egg_time_application
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
