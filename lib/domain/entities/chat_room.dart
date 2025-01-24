360ce3c27cbdd814181c8c59e3b2073636402b78
ljg7123 <ljg7123@daum.net>
2025-01-24 13:58:35 +0900

    feat: ChatRoom 엔티티 정의

---
+++ lib/domain/entities/chat_room.dart
@@ -0,0 +1,19 @@
+class ChatRoom {
+  final String id;
+  final String buyer;
+  final String seller;
+  final int unreadMessageCount;
+  final String? lastMessage;
+  final String? lastMessageTime;
+  final String? lastMessageSender;
+
+  ChatRoom({
+    required this.id,
+    required this.buyer,
+    required this.seller,
+    required this.unreadMessageCount,
+    this.lastMessage,
+    this.lastMessageTime,
+    this.lastMessageSender,
+  });
+}
