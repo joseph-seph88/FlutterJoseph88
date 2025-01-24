13961d9bf97552831de29229e6f377fecb3c3c74
ljg7123 <ljg7123@daum.net>
2025-01-24 17:07:18 +0900

    feat: 채팅 목록 필터 기능 구현

--- lib/presentation/screens/chat/chat_list_screen.dart
+++ lib/presentation/screens/chat/chat_list_screen.dart
@@ -1,24 +1,86 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:o2/domain/entities/chat_room.dart';
 import 'package:o2/presentation/providers/chat_provider.dart';
 import 'package:o2/presentation/screens/chat/widgets/chat_room_list.dart';
 
-class ChatListScreen extends ConsumerWidget {
+class ChatListScreen extends ConsumerStatefulWidget {
   const ChatListScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final chatRooms = ref.watch(chatRoomProvider);
+  ConsumerState createState() => _ChatListScreenState();
+}
+
+enum FilterType { all, selling, buying }
+
+class _ChatListScreenState extends ConsumerState<ChatListScreen> {
+  FilterType _filterType = FilterType.all;
+
+  @override
+  Widget build(BuildContext context) {
     final userId = 'buyer';
+    final chatRooms = ref
+        .watch(chatRoomProvider)
+        .where(
+            (element) => _shouldIncludeChatRoom(_filterType, userId, element))
+        .toList();
 
     return Scaffold(
       appBar: AppBar(title: Text('채팅')),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          // 필터 추가 예정
-          Expanded(child: ChatRoomList(chatRoomList: chatRooms, userId: userId)),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 12),
+            child: _buildFilterChips(),
+          ),
+          Expanded(
+              child: ChatRoomList(chatRoomList: chatRooms, userId: userId)),
         ],
       ),
     );
+  }
+
+  Widget _buildFilterChips() {
+    return Wrap(
+      spacing: 4,
+      children: FilterType.values.map((filter) {
+        return ChoiceChip(
+          label: Text(_filterLabel(filter)),
+          selected: _filterType == filter,
+          showCheckmark: false,
+          onSelected: (selected) {
+            if (selected) {
+              setState(() {
+                _filterType = filter;
+              });
+            }
+          },
+        );
+      }).toList(),
+    );
+  }
+
+  String _filterLabel(FilterType type) {
+    switch (type) {
+      case FilterType.all:
+        return '전체';
+      case FilterType.selling:
+        return '판매';
+      case FilterType.buying:
+        return '구매';
+    }
+  }
+
+  bool _shouldIncludeChatRoom(
+      FilterType type, String userId, ChatRoom chatRoom) {
+    switch (type) {
+      case FilterType.all:
+        return true;
+      case FilterType.selling:
+        return chatRoom.seller == userId;
+      case FilterType.buying:
+        return chatRoom.buyer == userId;
+    }
   }
 }
