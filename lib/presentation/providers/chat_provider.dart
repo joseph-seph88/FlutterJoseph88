3e51915b77525ff94c26e1cc25690828fa8ca88d
ljg7123 <ljg7123@daum.net>
2025-01-31 11:48:12 +0900

    feat: chatMessageProvider에 더미데이터 추가

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:o2/data/repositories/chat_repository_impl.dart';
+import 'package:o2/domain/entities/chat_message.dart';
 import 'package:o2/domain/entities/chat_room.dart';
 import 'package:o2/domain/usecases/chat_usecases.dart';
 
@@ -24,4 +25,43 @@ class ChatRoomNotifier extends StateNotifier<List<ChatRoom>> {
       state = data;
     });
   }
+}
+
+final chatMessageProvider =
+    StateNotifierProvider<ChatMessageNotifier, List<ChatMessage>>(
+  (ref) => ChatMessageNotifier(),
+);
+
+class ChatMessageNotifier extends StateNotifier<List<ChatMessage>> {
+  ChatMessageNotifier()
+      : super([
+          ChatMessage(
+            id: '0',
+            senderId: 'b',
+            type: ChatMessageType.text,
+            content: 'hi',
+            sentTime: DateTime(2025, 1, 26),
+          ),
+          ChatMessage(
+            id: '1',
+            senderId: 'b',
+            type: ChatMessageType.text,
+            content: '안녕하세요?',
+            sentTime: DateTime(2025, 1, 26),
+          ),
+          ChatMessage(
+            id: '2',
+            senderId: 'a',
+            type: ChatMessageType.text,
+            content: 'hello',
+            sentTime: DateTime.now(),
+          ),
+          ChatMessage(
+            id: '3',
+            senderId: 'b',
+            type: ChatMessageType.text,
+            content: 'zzz',
+            sentTime: DateTime.now(),
+          ),
+        ]);
 }
