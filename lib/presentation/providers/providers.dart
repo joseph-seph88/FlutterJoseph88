afc39840690b5069096f0d9717ca93dd1ccd094e
ljg7123 <ljg7123@daum.net>
2025-01-31 15:15:35 +0900

    refactor: data 레이어에 있는 Provider 를 presentation 레이어로 이동

---
+++ lib/presentation/providers/providers.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:o2/data/datasources/chat_remote_data_source.dart';
+import 'package:o2/data/repositories/chat_repository_impl.dart';
+import 'package:o2/domain/repositories/chat_repository.dart';
+import 'package:o2/domain/usecases/chat_usecases.dart';
+
+final chatRemoteDataSourceProvider = Provider<ChatRemoteDataSource>(
+    (ref) => ChatRemoteDataSourceImpl());
+
+final chatRepositoryProvider = Provider<ChatRepository>(
+    (ref) => ChatRepositoryImpl(ref.read(chatRemoteDataSourceProvider)));
+
+final getChatRoomsUseCaseProvider = Provider<GetChatRoomsUseCase>(
+    (ref) => GetChatRoomsUseCase(ref.read(chatRepositoryProvider)));
+
+final getChatMessagesUseCaseProvider = Provider<GetChatMessagesUseCase>(
+    (ref) => GetChatMessagesUseCase(ref.read(chatRepositoryProvider)));
