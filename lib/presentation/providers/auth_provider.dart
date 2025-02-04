189e47ae7595fce2fb7ea48e8566465727888858
GeonHo Kim <yorjsgh@naver.com>
2025-02-04 19:36:19 +0900

    feat : 이메일 중복체크 기능 추가가

--- lib/presentation/providers/auth_provider.dart
+++ lib/presentation/providers/auth_provider.dart
@@ -2,11 +2,10 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:o2/data/datasources/auth_data_source.dart';
 import 'package:o2/data/datasources/user_data_source.dart';
 import 'package:o2/data/repositories/auth_repository_impl.dart';
+import 'package:o2/domain/entities/user_entity.dart';
 import 'package:o2/domain/usecases/auth_use_case.dart';
 
-import '../../data/models/user_model.dart';
-
-final authProvider = StateNotifierProvider<AuthNotifier, UserModel?>(
+final authProvider = StateNotifierProvider<AuthNotifier, UserEntity?>(
   (ref) => AuthNotifier(
     AuthUseCase(
       AuthRepositoryImpl(AuthDataSource(), UserDataSource()),
@@ -14,7 +13,7 @@ final authProvider = StateNotifierProvider<AuthNotifier, UserModel?>(
   ),
 );
 
-class AuthNotifier extends StateNotifier<UserModel?> {
+class AuthNotifier extends StateNotifier<UserEntity?> {
   final AuthUseCase authUseCase;
 
   AuthNotifier(this.authUseCase) : super(null);
@@ -30,9 +29,18 @@ class AuthNotifier extends StateNotifier<UserModel?> {
     }
   }
 
-  Future<void> signUp(String email, String password) async {
-    final user = await authUseCase.signUp(email, password);
+  Future<bool> signUp(UserEntity userEntity, String password) async {
+    final user = await authUseCase.signUp(userEntity, password);
 
-    if (user != null) state = user;
+    if (user != null) {
+      state = user;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Future<bool> validEmail(String email) async {
+    return authUseCase.validEmail(email);
   }
 }
