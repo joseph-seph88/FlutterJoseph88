6acdc049aa1132318f7d4a3ee22f1b2c05841684
GeonHo Kim <yorjsgh@naver.com>
2025-02-04 19:35:22 +0900

    feat : user 프로퍼티 추가

--- lib/data/models/user_model.dart
+++ lib/data/models/user_model.dart
@@ -1,29 +1,37 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:o2/domain/entities/user_entity.dart';
 
 class UserModel {
   final String id;
   final String email;
+  final String name;
 
-  UserModel({required this.id, required this.email});
-
-  factory UserModel.fromFirebaseUser(User user) {
-    return UserModel(
-      id: user.uid,
-      email: user.email ?? "",
-    );
-  }
+  UserModel({
+    required this.id,
+    required this.email,
+    required this.name,
+  });
 
   factory UserModel.fromJson(Map<String, dynamic> json) {
     return UserModel(
-      id: json['id'],
-      email: json['email'],
+      id: json["id"],
+      email: json["email"],
+      name: json["name"],
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
-      'id': id,
-      'email': email,
+      "id": id,
+      "email": email,
+      "name": name,
     };
+  }
+
+  UserEntity toEntity() {
+    return UserEntity(
+      id: id,
+      email: email,
+      name: name,
+    );
   }
 }
--- lib/domain/entities/user_entity.dart
+++ lib/domain/entities/user_entity.dart
@@ -1,6 +1,21 @@
+import 'package:o2/data/models/user_model.dart';
+
 class UserEntity {
   final String id;
   final String email;
+  final String name;
 
-  UserEntity({required this.id, required this.email});
+  UserEntity({
+    required this.id,
+    required this.email,
+    required this.name,
+  });
+
+  UserModel toModel(String id) {
+    return UserModel(
+      id: id,
+      email: email,
+      name: name,
+    );
+  }
 }
