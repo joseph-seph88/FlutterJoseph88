d4587e46aeb2ffb89deba8b1c34539f4aa28d965
GeonHo Kim <yorjsgh@naver.com>
2025-02-04 19:36:45 +0900

    feat : 텍스트 필드 위젯 에러메시지 추가

--- lib/presentation/screens/auth/widgets/auth_text_field.dart
+++ lib/presentation/screens/auth/widgets/auth_text_field.dart
@@ -11,6 +11,7 @@ class AuthTextField extends StatelessWidget {
     this.obscureText,
     this.onSuffixIconPressed,
     this.focusNode,
+    this.errorText,
   });
 
   final TextEditingController controller;
@@ -19,6 +20,7 @@ class AuthTextField extends StatelessWidget {
   final TextInputType? keyboardType;
   final bool? obscureText;
   final FocusNode? focusNode;
+  final String? errorText;
 
   @override
   Widget build(BuildContext context) {
@@ -50,14 +52,15 @@ class AuthTextField extends StatelessWidget {
       decoration: InputDecoration(
         border: const OutlineInputBorder(),
         hintText: hintText,
+        errorText: errorText,
         prefixIcon: Icon(icon),
         suffixIcon: onSuffixIconPressed == null
             ? null
             : IconButton(
                 onPressed: onSuffixIconPressed,
                 icon: obscureText!
-                    ? Icon(Icons.visibility_off)
-                    : Icon(Icons.visibility),
+                    ? const Icon(Icons.visibility_off)
+                    : const Icon(Icons.visibility),
               ),
       ),
       focusNode: focusNode,
