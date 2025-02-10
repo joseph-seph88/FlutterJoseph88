f6d1cd2ac5a60e51a1c5cf34d4c800b6663263f0
JJH <wlsgur4033@gmail.com>
2025-02-10 10:27:17 +0900

    feat. image 압축 패키지 init

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:o2/core/theme/app_theme.dart';
 import 'package:o2/core/utils/permission_manager.dart';
 import 'package:o2/data/datasources/product_data_source.dart';
@@ -13,6 +14,10 @@ import 'route.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // flutter_image_compress 초기화
+  FlutterImageCompress.validator.ignoreCheckExtName = true;
+
   await Future.wait([
     NaverMapSdk.instance.initialize(),
     PermissionManager().requestLocationPermission(),
@@ -20,7 +25,6 @@ void main() async {
       options: DefaultFirebaseOptions.currentPlatform,
     ),
   ]);
-
 
   // 더미 데이터 업로드
   // await uploadDummyData();
