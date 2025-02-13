d567066e9feb9875a449aa3046603a56e9d6df11
ljg7123 <ljg7123@daum.net>
2025-02-13 11:31:50 +0900

    refactor: 채팅 목록 화면 코드 리팩토링
    1. 채팅 목록을 Stream을 구독하여 상태 변경하도록 수정
    2. 필터링 로직 ViewModel로 이동

--- lib/presentation/screens/chat/chat_room_list_view_model.dart
+++ lib/presentation/screens/chat/chat_room_list_view_model.dart
@@ -1,31 +1,59 @@
+import 'dart:async';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:o2/domain/entities/chat_room.dart';
+import 'package:o2/domain/usecases/chat_use_case.dart';
 import 'package:o2/presentation/providers/auth_provider.dart';
-import 'package:o2/presentation/providers/chat_provider.dart';
+import 'package:o2/presentation/providers/providers.dart';
+import 'package:o2/presentation/screens/chat/chat_list_screen.dart';
 
 class ChatRoomListViewModel extends StateNotifier<AsyncValue<List<ChatRoom>>> {
   final Ref ref;
+  final GetChatRoomsUseCase getChatRoomsUseCase;
+  StreamSubscription? _subscription;
 
-  ChatRoomListViewModel(this.ref) : super(const AsyncLoading()) {
-    _loadChatRooms();
+  ChatRoomListViewModel(this.ref, this.getChatRoomsUseCase)
+      : super(const AsyncLoading()) {
+    _listenChatRoomStream();
   }
 
-  Future<void> _loadChatRooms() async {
+  @override
+  void dispose() {
+    _subscription?.cancel();
+    super.dispose();
+  }
+
+  void _listenChatRoomStream() {
+    _subscription?.cancel();
     final userId = ref.read(authProvider)?.id;
     if (userId == null) {
       state = const AsyncData([]);
       return;
     }
 
-    try {
-      state = ref.watch(chatRoomStreamProvider);
-    } catch (e, stackTrace) {
+    _subscription = getChatRoomsUseCase(userId).listen((data) {
+      state = AsyncData(data);
+    }, onError: (e, stackTrace) {
       state = AsyncError(e, stackTrace);
-    }
+    });
+  }
+
+  List<ChatRoom> getFilteredChatRooms(FilterType type) {
+    final userId = ref.read(authProvider)?.id;
+    if (userId == null) return [];
+
+
+    return state.value?.where((element) {
+      return switch (type) {
+        FilterType.all => true,
+        FilterType.selling => userId == element.seller,
+        FilterType.buying => userId == element.buyer,
+      };
+    }).toList() ?? [];
   }
 }
 
 final chatRoomListViewModelProvider = StateNotifierProvider.autoDispose<
     ChatRoomListViewModel, AsyncValue<List<ChatRoom>>>((ref) {
-  return ChatRoomListViewModel(ref);
+  return ChatRoomListViewModel(ref, ref.read(getChatRoomsUseCaseProvider));
 });
