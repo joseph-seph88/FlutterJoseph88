44189c6d316e5b6ba54d818a33a5ef374572dfde
Joseph <koutz3434@naver.com>
2025-06-25 14:31:58 +0900

    250625 Community-Board BASIC API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
@@ -7,6 +8,19 @@ import { CommunityBoardModule } from './community-board/community-board.module';
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [ConfigModule.forRoot(), CommunityBoardModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'joseph',
+      password: 'admin123!',
+      database: 'nest_test',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    CommunityBoardModule
+  ],
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(morgan('dev'));
   const config = app.get(ConfigService);
   const port = config.get('PORT', 3000);
   const ip = config.get('SERVER_IP', '127.0.0.1');
